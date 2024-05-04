7d3662806b78d33db48a22010e20c8a7c92e0c8c
giorgi568 <mateshvili02@mail.ru>
2024-05-04 21:23:17 +0400

    added populatedbdImage (not finished)

--- populatedbImage.js
+++ populatedbImage.js
@@ -9,8 +9,6 @@ console.log(
 const userArgs = process.argv.slice(2);
 const Image = require('./models/image');
 
-const images = [];
-
 const mongoose = require('mongoose');
 mongoose.set('strictQuery', false);
 
@@ -23,20 +21,40 @@ async function main() {
   console.log('about to connect');
   await mongoose.connect(mongoDB);
   console.log('should be connected');
+  await createImages();
+  console.log('closing mongoose');
+  mongoose.connection.close();
 }
 
-async function imageCreate() {
-  const directory = '/images';
+async function imageCreate(file, fileTitle, cordX, cordY) {
   try {
-    const files = fs.readdirSync(directory);
+    if (path.extname(file) === '.jpg' || path.extname(file) === '.png') {
+      const filePath = path.join(directory, file);
 
-    for (const file of files) {
-      if (path.extname(file) === '.jpg' || path.extname(file) === '.png') {
-        const filePath = path.join(directory, file);
-        
-      }
+      const imageBuffer = fs.readFileSync(filePath);
+
+      const newImage = new Image({
+        title: fileTitle,
+        data: imageBuffer,
+        contentType: path.extname(file).startsWith('.jpg')
+          ? 'image/jpg'
+          : 'image/png',
+        cordX,
+        cordY,
+      });
+
+      await newImage.save();
+      console.log('image saved : ' + fileTitle);
     }
   } catch (err) {
     console.log('error uploading image: ' + err);
   }
+}
+async function createImages() {
+  const directory = '/images';
+  const files = fs.readdirSync(directory);
+
+  console.log('adding images');
+
+  await Promise.all([imageCreate(files[0], 'holywood', 50, 50)]);
 }
