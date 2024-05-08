0fabd352157494b9fa907164d987f0526816c3a8
giorgi568 <mateshvili02@mail.ru>
2024-05-09 02:30:07 +0400

    changed way to upload images to the database

--- models/image.js
+++ models/image.js
@@ -4,8 +4,7 @@ const Schema = mongoose.Schema;
 
 const imageSchema = new Schema({
   title: { type: String, required: true },
-  data: { type: Buffer, required: true },
-  contentType: { type: String, required: true },
+  url: { type: String, required: true },
   cordX_waldo: { type: Number, required: true },
   cordY_waldo: { type: Number, required: true },
   cordX_wenda: { type: Number },
--- populatedbImage.js
+++ populatedbImage.js
@@ -1,6 +1,9 @@
 #! /usr/bin/env node
-const fs = require('fs');
-const path = require('path');
+// const fs = require('fs');
+// const path = require('path');
+
+// no longer using commented out code because storing images
+// in mongodb directly was a mistake
 
 console.log(
   'Specified database as argument - e.g.: node populatedb "mongodb+srv://cooluser:coolpassword@cluster0.lz91hw2.mongodb.net/local_library?retryWrites=true&w=majority"'
@@ -29,53 +32,95 @@ async function main() {
 // its way more complicated that it should be
 // but simpler than writing it twice
 
-const directory = 'public/images';
+// const directory = 'public/images';
+
+// async function imageCreate(
+//   file,
+//   fileTitle,
+//   cordX_waldo,
+//   cordY_waldo,
+//   cordX_wenda,
+//   cordY_wenda
+// ) {
+//   try {
+//     if (path.extname(file) === '.jpg' || path.extname(file) === '.png') {
+//       const filePath = path.join(directory, file);
+
+//       const imageBuffer = fs.readFileSync(filePath);
+
+// const newImage = new Image({
+//   title: fileTitle,
+//   data: imageBuffer,
+//   contentType: path.extname(file).startsWith('.jpg')
+//     ? 'image/jpg'
+//     : 'image/png',
+//   cordX_waldo,
+//   cordY_waldo,
+// });
+
+// if (cordX_wenda !== undefined) {
+//   newImage.cordX_wenda = cordX_wenda;
+//   newImage.cordY_wenda = cordY_wenda;
+// }
+
+// await newImage.save();
+// console.log('image saved : ' + fileTitle);
+//     }
+//   } catch (err) {
+//     console.log('error uploading image: ' + err);
+//   }
+// }
 
 async function imageCreate(
-  file,
-  fileTitle,
+  url,
+  title,
   cordX_waldo,
   cordY_waldo,
   cordX_wenda,
   cordY_wenda
 ) {
   try {
-    if (path.extname(file) === '.jpg' || path.extname(file) === '.png') {
-      const filePath = path.join(directory, file);
+    const newImage = new Image({
+      url,
+      title,
+      cordX_waldo,
+      cordY_waldo,
+    });
 
-      const imageBuffer = fs.readFileSync(filePath);
-
-      const newImage = new Image({
-        title: fileTitle,
-        data: imageBuffer,
-        contentType: path.extname(file).startsWith('.jpg')
-          ? 'image/jpg'
-          : 'image/png',
-        cordX_waldo,
-        cordY_waldo,
-      });
-
-      if (cordX_wenda !== undefined) {
-        newImage.cordX_wenda = cordX_wenda;
-        newImage.cordY_wenda = cordY_wenda;
-      }
-
-      await newImage.save();
-      console.log('image saved : ' + fileTitle);
+    if (cordX_wenda !== undefined) {
+      newImage.cordX_wenda = cordX_wenda;
+      newImage.cordY_wenda = cordY_wenda;
     }
+
+    await newImage.save();
+    console.log('image saved : ' + title);
   } catch (err) {
-    console.log('error uploading image: ' + err);
+    console.log(err);
   }
 }
 
 async function createImages() {
-  const files = fs.readdirSync(directory);
+  // const files = fs.readdirSync(directory);
 
   console.log('adding images');
 
   await Promise.all([
-    imageCreate(files[0], 'holywood', 844, 330, 710, 519),
-    imageCreate(files[1], 'battle', 827, 284),
-    imageCreate(files[2], 'skiing', 1027, 550, 590, 322),
+    imageCreate(
+      'https://i.postimg.cc/pXs8yK37/test.jpg',
+      'holywood',
+      844,
+      330,
+      710,
+      519
+    ),
+    imageCreate('https://i.postimg.cc/y8WRRkHN/test2.jpg', 'battle', 827, 284),
+    imageCreate(
+      'https://i.postimg.cc/mrHFdJhf/test3.jpg',
+      'skiing',
+      1027,
+      550,
+      590,
+      322
+    ),
   ]);
 }
